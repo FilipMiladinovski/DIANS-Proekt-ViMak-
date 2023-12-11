158a2afc2bd822b37a82daa2b4ff3d7435413c22
FilipMiladinovski <150627594+FilipMiladinovski@users.noreply.github.com>
2023-12-11 01:07:51 +0100

    Add files via upload

---
+++ Технички прототип/diansProject/src/main/java/finki/diansproject/WineRepository.java
@@ -0,0 +1,9 @@
+package finki.diansproject;
+
+import org.bson.types.ObjectId;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface WineRepository extends MongoRepository<Wine, ObjectId> {
+}
