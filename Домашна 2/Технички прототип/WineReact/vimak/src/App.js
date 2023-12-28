9317db63dd584734d64247b05701c7ce53bf750c
n1kkinG <63655374+n1kkinG@users.noreply.github.com>
2023-12-28 23:33:04 +0100

    git fix

--- Домашна 2/Технички прототип/WineReact/vimak/src/App.js
+++ Домашна 2/Технички прототип/WineReact/vimak/src/App.js
@@ -2,10 +2,15 @@ import './App.css';
 import api from './api/axiosConfig';
 import {useState, useEffect} from 'react';
 import Layout from './components/Layout';
-import {Routes, Route} from 'react-router-dom';
+import {Routes, Route, Router} from 'react-router-dom';
 import Home from './components/home/Home';
+import Login from './components/Login';
+import Register from './components/Register';
+import Wines from './components/Vina'
+
 
 function App() {
+  
  
   const [wines, setWines] = useState();
 
@@ -29,10 +34,13 @@ function App() {
 
   return (
     <div className="App">
+      
       <Routes>
-        <Route path="/" element={<Layout/>}>
-          <Route path="/" element={<Home wines={wines}/>}>
-          </Route>
+      <Route path="/" element={<Layout />}>
+            <Route path="/" element={<Home wines={wines} />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/register" element={<Register/>}/>
+            <Route path="/wines" element={<Wines/>}/>
         </Route>
       </Routes> 
 
